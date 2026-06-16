bce7c55a607d5367e59abd4e3e41ed8a5a81b79f
asimayazmrd0987 <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>
2026-06-16 22:55:10 +0500

    jb__6

--- Arrays/array1.c
+++ Arrays/array1.c
@@ -7,12 +7,12 @@ int main()
 	int *k,*o;
 	k=&marks[2];
 	o=(marks+2);
-	if(k==o){
+	if(k==0){
 		printf("Same\n");
 	}
 	else{printf("Not Same\n");}
 	
-	//printf("The address is  %d\n", *(j + 1));
+	printf("The address is  %d\n", *(j + 1));
 	//printf("the 0 and 1 marks is %d and %d", marks[0], marks[1]);
 	return 0;
 }
